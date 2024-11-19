1ee9c8268639b70024055ded431414dd05247a8d
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-11-20 01:06:22 +0600

    model create

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const jwt = require('jsonwebtoken');
 const UserModel = require("../models/UsersModel");
 
 exports.registration = (req, res) => {
@@ -10,3 +12,40 @@ exports.registration = (req, res) => {
             res.status(500).json({ status: "error", data: err })
         })
 }
+
+// Ensure jwt is imported
+
+exports.login = (req, res) => {
+    let reqBody = req.body;
+
+    UserModel.aggregate([
+        { $match: reqBody },
+        { $project: { _id: 0, email: 1, firstName: 1, lastName: 1, mobile: 1, photo: 1 } }
+    ])
+        .then(data => {
+            if (data.length > 0) {
+                let payload = { exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60), data: data[0]['email'] };
+                let token = jwt.sign(payload, 'SecretKey123456789');
+                res.status(200).json({ status: "success", token: token, data: data[0] });
+            } else {
+                res.status(401).json({ status: "unauthorized" });
+            }
+        })
+        .catch(err => {
+            res.status(400).json({ status: "fail", data: err });
+        });
+};
+
+
+exports.profileUpdate = (req, res) => {
+    let email = req.headers['email'];
+    let reqBody = req.body;
+
+    UserModel.updateOne({ email: email }, reqBody)
+        .then(data => {
+            res.status(200).json({ status: "success", data: data });
+        })
+        .catch(err => {
+            res.status(400).json({ status: "fail", data: err });
+        });
+};
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,22 @@
 const express = require('express');
 
 const router = express.Router();
-const UserController = require('../controllers/UserController')
+const UserController = require('../controllers/UserController');
+const AuthVerifyMiddleware = require('../middlewars/AuthVerifyMiddleware');
+const TaskController = require('../controllers/TaskController');
 
+// user Profile
 router.post("/registration", UserController.registration)
+router.post("/login", UserController.login)
+router.post("/profileUpdate", AuthVerifyMiddleware, UserController.profileUpdate)
+
+// User Tasks
+router.post("/createTask", AuthVerifyMiddleware, TaskController.createTask)
+router.get("/updateTaskStatus/:id/:status", AuthVerifyMiddleware, TaskController.updateTaskStatus)
+router.post("/listTaskByStatus/:status", AuthVerifyMiddleware, TaskController.listTaskByStatus)
+router.get("/taskStatusCount", AuthVerifyMiddleware, TaskController.taskStatusCount)
+router.post("/deleteTask/:id", AuthVerifyMiddleware, TaskController.deleteTask)
+
 
 
 module.exports = router
